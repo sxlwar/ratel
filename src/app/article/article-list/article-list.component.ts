0407421a58f02553e1cfd94e369c3254ea8d2690
ZQ-jhon <350037310@qq.com>
2018-12-01 22:45:27 +0800

    fix: :bug: fix issue #6 #10 #11

--- src/app/article/article-list/article-list.component.ts
+++ src/app/article/article-list/article-list.component.ts
@@ -1,10 +1,8 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { ChangeDetectionStrategy, Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
-
-import { Observable, Subject, merge } from 'rxjs';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { merge, Observable, Subject } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { ArticleOverview } from 'src/app/interface/response.interface';
-
+import { ArticleAnimation } from '../article.animation';
 import { ArticleService } from '../providers/article.service';
 
 @Component({
@@ -12,13 +10,7 @@ import { ArticleService } from '../providers/article.service';
     templateUrl: './article-list.component.html',
     styleUrls: ['./article-list.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    animations: [
-        trigger('flyInOut', [
-            state('in', style({ transform: 'translateX(0)' })),
-            transition('void => *', [style({ transform: 'translateX(-100%)' }), animate(500)]),
-            transition('* => void', [animate(500, style({ transform: 'translateX(-100%)' }))]),
-        ]),
-    ],
+    animations: [ArticleAnimation],
 })
 export class ArticleListComponent implements OnInit {
     @Input()
@@ -36,11 +28,10 @@ export class ArticleListComponent implements OnInit {
 
     show: Observable<boolean>;
 
-    constructor(private _articleService: ArticleService) {}
+    constructor(private _articleService: ArticleService) { }
 
     ngOnInit() {
         this.list = this.search.pipe(switchMap(value => this._articleService.getArticlesOverview({ title: value })));
-
         this.show = merge(this.display, this.close$.asObservable());
     }
 }
