b803d08cc00f2041149b0aaf1800ae8e904b7c7b
shaoxuelei <sxlwar@163.com>
2019-03-26 11:27:41 +0800

    fix emoji

--- src/app/providers/emoji.service.ts
+++ src/app/providers/emoji.service.ts
@@ -1,182 +1,180 @@
 import { Injectable } from '@angular/core';
 
-import {
-  CompressedEmojiData,
-  EmojiData,
-  EmojiVariation,
-  Emoji
-} from '../interface/emoji.interfaces';
+import { CompressedEmojiData, EmojiData, EmojiVariation, Emoji } from '../interface/emoji.interfaces';
 import { emojis } from '../constant/emoji/emojis';
 
 const COLONS_REGEX = /^(?:\:([^\:]+)\:)(?:\:skin-tone-(\d)\:)?$/;
 const SKINS = ['1F3FA', '1F3FB', '1F3FC', '1F3FD', '1F3FE', '1F3FF'];
-export const DEFAULT_BACKGROUND_FN = (
-  set: string,
-  sheetSize: number,
-) => `https://unpkg.com/emoji-datasource-${set}@4.0.4/img/${set}/sheets-256/${sheetSize}.png`;
+export const DEFAULT_BACKGROUND_FN = (set: string, sheetSize: number) =>
+    `https://unpkg.com/emoji-datasource-${set}@4.0.4/img/${set}/sheets-256/${sheetSize}.png`;
 
 @Injectable({ providedIn: 'root' })
 export class EmojiService {
-  names: { [key: string]: EmojiData } = {};
-  emojis: EmojiData[] = [];
+    names: { [key: string]: EmojiData } = {};
+    emojis: EmojiData[] = [];
 
-  constructor() {
-      this.uncompress(emojis);
-  }
+    constructor() {
+        this.uncompress(emojis);
+    }
 
-  private uncompress(list: CompressedEmojiData[]) {
-    this.emojis = list.map(emoji => {
-      const data: any = { ...emoji };
-      if (!data.shortNames) {
-        data.shortNames = [];
-      }
-      data.shortNames.unshift(data.shortName);
-      data.id = data.shortName;
-      data.native = this.unifiedToNative(data.unified);
+    private uncompress(list: CompressedEmojiData[]) {
+        this.emojis = list.map(emoji => {
+            const data: any = { ...emoji };
+            if (!data.shortNames) {
+                data.shortNames = [];
+            }
+            data.shortNames.unshift(data.shortName);
+            data.id = data.shortName;
+            data.native = this.unifiedToNative(data.unified);
 
-      if (!data.skinVariations) {
-        data.skinVariations = [];
-      }
+            if (!data.skinVariations) {
+                data.skinVariations = [];
+            }
 
-      if (!data.keywords) {
-        data.keywords = [];
-      }
+            if (!data.keywords) {
+                data.keywords = [];
+            }
 
-      if (!data.emoticons) {
-        data.emoticons = [];
-      }
+            if (!data.emoticons) {
+                data.emoticons = [];
+            }
 
-      if (!data.hidden) {
-        data.hidden = [];
-      }
+            if (!data.hidden) {
+                data.hidden = [];
+            }
 
-      if (!data.text) {
-        data.text = '';
-      }
+            if (!data.text) {
+                data.text = '';
+            }
 
-      if (data.obsoletes) {
-        // get keywords from emoji that it obsoletes since that is shared
-        const f = list.find(x => x.unified === data.obsoletes);
-        if (f) {
-          if (f.keywords) {
-            data.keywords = [...data.keywords, ...f.keywords, f.shortName];
-          } else {
-            data.keywords = [...data.keywords, f.shortName];
-          }
+            if (data.obsoletes) {
+                // get keywords from emoji that it obsoletes since that is shared
+                const f = list.find(x => x.unified === data.obsoletes);
+                if (f) {
+                    if (f.keywords) {
+                        data.keywords = [...data.keywords, ...f.keywords, f.shortName];
+                    } else {
+                        data.keywords = [...data.keywords, f.shortName];
+                    }
+                }
+            }
+
+            this.names[data.unified] = data;
+            for (const n of data.shortNames) {
+                this.names[n] = data;
+            }
+            return data;
+        });
+    }
+
+    private getData(emoji: EmojiData | string, skin?: Emoji['skin'], set?: Emoji['set']): EmojiData | null {
+        let emojiData: any;
+
+        if (typeof emoji === 'string') {
+            const matches = emoji.match(COLONS_REGEX);
+
+            if (matches) {
+                emoji = matches[1];
+
+                if (matches[2]) {
+                    skin = parseInt(matches[2], 10) as Emoji['skin'];
+                }
+            }
+            if (this.names.hasOwnProperty(emoji)) {
+                emojiData = this.names[emoji];
+            } else {
+                return null;
+            }
+        } else if (emoji.id) {
+            emojiData = this.names[emoji.id];
+        } else if (emoji.unified) {
+            emojiData = this.names[emoji.unified.toUpperCase()];
         }
-      }
 
-      this.names[data.unified] = data;
-      for (const n of data.shortNames) {
-        this.names[n] = data;
-      }
-      return data;
-    });
-  }
-
-  private getData( emoji: EmojiData | string, skin?: Emoji['skin'], set?: Emoji['set']): EmojiData | null {
-    let emojiData: any;
-
-    if (typeof emoji === 'string') {
-      const matches = emoji.match(COLONS_REGEX);
-
-      if (matches) {
-        emoji = matches[1];
-
-        if (matches[2]) {
-          skin = parseInt(matches[2], 10) as Emoji['skin'];
+        if (!emojiData) {
+            emojiData = emoji;
+            emojiData.custom = true;
         }
-      }
-      if (this.names.hasOwnProperty(emoji)) {
-        emojiData = this.names[emoji];
-      } else {
-        return null;
-      }
-    } else if (emoji.id) {
-      emojiData = this.names[emoji.id];
-    } else if (emoji.unified) {
-      emojiData = this.names[emoji.unified.toUpperCase()];
+
+        const hasSkinVariations = emojiData.skinVariations && emojiData.skinVariations.length;
+        if (hasSkinVariations && skin && skin > 1 && set) {
+            emojiData = { ...emojiData };
+
+            const skinKey = SKINS[skin - 1];
+            const variationData = emojiData.skinVariations.find((n: EmojiVariation) => n.unified.includes(skinKey));
+
+            if (!variationData.hidden || !variationData.hidden.includes(set)) {
+                emojiData.skinTone = skin;
+                emojiData = { ...emojiData, ...variationData };
+            }
+            emojiData.native = this.unifiedToNative(emojiData.unified);
+        }
+
+        emojiData.set = set || '';
+        return emojiData as EmojiData;
     }
 
-    if (!emojiData) {
-      emojiData = emoji;
-      emojiData.custom = true;
+    private unifiedToNative(unified: string) {
+        const codePoints = unified.split('-').map(u => parseInt(`0x${u}`, 16));
+        return String.fromCodePoint(...codePoints);
     }
 
-    const hasSkinVariations = emojiData.skinVariations && emojiData.skinVariations.length;
-    if (hasSkinVariations && skin && skin > 1 && set) {
-      emojiData = { ...emojiData };
-
-      const skinKey = SKINS[skin - 1];
-      const variationData = emojiData.skinVariations.find((n: EmojiVariation) =>
-        n.unified.includes(skinKey),
-      );
-
-      if (!variationData.hidden || !variationData.hidden.includes(set)) {
-        emojiData.skinTone = skin;
-        emojiData = { ...emojiData, ...variationData };
-      }
-      emojiData.native = this.unifiedToNative(emojiData.unified);
-    }
-
-    emojiData.set = set || '';
-    return emojiData as EmojiData;
-  }
-
-  private unifiedToNative(unified: string) {
-    const codePoints = unified.split('-').map(u => parseInt(`0x${u}`, 16));
-    return String.fromCodePoint(...codePoints);
-  }
-
-  emojiSpriteStyles(
-    sheet: EmojiData['sheet'],
-    set: Emoji['set'] = 'apple',
-    size: Emoji['size'] = 24,
-    sheetSize: Emoji['sheetSize'] = 64,
-    backgroundImageFn: Emoji['backgroundImageFn'] = DEFAULT_BACKGROUND_FN,
-    sheetColumns = 52,
+    emojiSpriteStyles(
+        sheet: EmojiData['sheet'],
+        set: Emoji['set'] = 'apple',
+        size: Emoji['size'] = 24,
+        sheetSize: Emoji['sheetSize'] = 64,
+        backgroundImageFn: Emoji['backgroundImageFn'] = DEFAULT_BACKGROUND_FN,
+        sheetColumns = 52,
     ) {
-    return {
-      width: `${size}px`,
-      height: `${size}px`,
-      display: 'inline-block',
-      'background-image': `url(${backgroundImageFn(set, sheetSize)})`,
-      'background-size': `${100 * sheetColumns}%`,
-      'background-position': this.getSpritePosition(sheet, sheetColumns),
-    };
-  }
-
-  private getSpritePosition(sheet: EmojiData['sheet'], sheetColumns: number) {
-    const [sheet_x, sheet_y] = sheet;
-    const multiply = 100 / (sheetColumns - 1);
-    return `${multiply * sheet_x}% ${multiply * sheet_y}%`;
-  }
-
-  private sanitize(emoji: EmojiData | null): EmojiData | null {
-    if (emoji === null) {
-      return null;
+        return {
+            width: `${size}px`,
+            height: `${size}px`,
+            display: 'inline-block',
+            'background-image': `url(${backgroundImageFn(set, sheetSize)})`,
+            'background-size': `${100 * sheetColumns}%`,
+            'background-position': this.getSpritePosition(sheet, sheetColumns),
+        };
     }
-    const id = emoji.id || emoji.shortNames[0];
-    let colons = `:${id}:`;
-    if (emoji.skinTone) {
-      colons += `:skin-tone-${emoji.skinTone}:`;
+
+    private getSpritePosition(sheet: EmojiData['sheet'], sheetColumns: number) {
+        const [sheet_x, sheet_y] = sheet;
+        const multiply = 100 / (sheetColumns - 1);
+        return `${multiply * sheet_x}% ${multiply * sheet_y}%`;
     }
-    emoji.colons = colons;
-    return { ...emoji };
-  }
 
-  private getSanitizedData( emoji: string | EmojiData, skin?: Emoji['skin'], set?: Emoji['set']) {
-    return this.sanitize(this.getData(emoji, skin, set));
-  }
+    private sanitize(emoji: EmojiData | null): EmojiData | null {
+        if (emoji === null) {
+            return null;
+        }
+        const id = emoji.id || emoji.shortNames[0];
+        let colons = `:${id}:`;
+        if (emoji.skinTone) {
+            colons += `:skin-tone-${emoji.skinTone}:`;
+        }
+        emoji.colons = colons;
+        return { ...emoji };
+    }
 
-  /**
-   * @param emoji Emoji name
-   * @returns Css value for emoji;
-   */
-  getStyle(emoji: string): string {
-      const data = this.getData(emoji);
-      const spriteStyle = this.emojiSpriteStyles(data.sheet, 'apple', 18, 64, DEFAULT_BACKGROUND_FN, 52);
+    private getSanitizedData(emoji: string | EmojiData, skin?: Emoji['skin'], set?: Emoji['set']) {
+        return this.sanitize(this.getData(emoji, skin, set));
+    }
 
-      return Object.entries(spriteStyle).reduce((pre, [key, value]) => `${pre}; ${key}: ${value}; `, 'margin: 0 5px 0 0');
-  }
+    /**
+     * @param emoji Emoji name
+     * @returns Css value for emoji;
+     */
+    getStyle(emoji: string): string {
+        const data = this.getData(emoji);
+        if (!data) {
+            return '';
+        } else {
+            const spriteStyle = this.emojiSpriteStyles(data.sheet, 'apple', 18, 64, DEFAULT_BACKGROUND_FN, 52);
+
+            return Object.entries(spriteStyle).reduce(
+                (pre, [key, value]) => `${pre}; ${key}: ${value}; `,
+                'margin: 0 5px 0 0',
+            );
+        }
+    }
 }
